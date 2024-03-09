995492e1c32a61ac9f2323f0b7bada4848039dfa
BrK08 <126277787+BrK08@users.noreply.github.com>
2024-03-09 18:53:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["By BrK , I l0VE SPACE🌌"];
 
 
 let currentIndex = 0;
